b5b488335ab533dbfbec1e6086629893d0fb62cd
MoR1uSs <vlad.kustov07@gmail.com>
2025-02-20 16:48:13 +0100

    background enhancing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,22 +7,46 @@ function scrollToSection(sectionId) {
 document.addEventListener("DOMContentLoaded", function () {
     const codeAnimation = document.getElementById("code-animation");
 
-    // Java Code for Background Effect
-    const javaCode = `public class HelloWorld {
+        const javaCode = `public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to My Portfolio!");
+        Greetings();
+    }
+
+    public static void Greetings() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Welcome to My Portfolio, " + name + "!");
+        scanner.close();
     }
 }`;
+    
+        let deleting = false;
+        let i = 0;
 
-    let i = 0;
-
-    function typeCode() {
-        if (i < javaCode.length) {
-            codeAnimation.innerHTML += javaCode.charAt(i);
-            i++;
-            setTimeout(typeCode, 30); // Adjust speed
+        function typeAndDelete(){
+            if(!deleting){
+                if(i < javaCode.length){
+                    codeAnimation.innerHTML += javaCode.charAt(i);
+                    i++;
+                    setTimeout(typeAndDelete, 30);
+                }
+                else{
+                    deleting = true;
+                    setTimeout(typeAndDelete, 1000);
+                }
+            } else {
+                if(i > 0){
+                    codeAnimation.innerHTML = javaCode.substring(0, i - 1);
+                    i--;
+                    setTimeout(typeAndDelete, 20);
+                }
+                else{
+                    deleting = false;
+                    setTimeout(typeAndDelete, 500);
+                }
+            }
         }
-    }
 
-    typeCode();
+    typeAndDelete();
 });
