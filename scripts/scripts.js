948d39e21f8b76a7d4f60057761ee10ac02ff57d
MoR1uSs <vlad.kustov07@gmail.com>
2025-03-31 08:31:32 +0200

    final commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,24 +20,30 @@ public class HelloWorld {
     let deleting = false;
     let i = 0;
 
-    function typeAndDelete(){
-        if(!deleting){
-            if(i < javaCode.length){
-                codeAnimation.innerHTML += javaCode.charAt(i);
+    function typeAndDelete() {
+        if (!deleting) {
+            if (i < javaCode.length) {
+                codeAnimation.innerHTML = Prism.highlight(
+                    javaCode.substring(0, i + 1), 
+                    Prism.languages.java, 
+                    'java'
+                );
                 i++;
                 setTimeout(typeAndDelete, 30);
-            }
-            else{
+            } else {
                 deleting = true;
-                setTimeout(typeAndDelete, 1000);
+                setTimeout(typeAndDelete, 10000);
             }
         } else {
-            if(i > 0){
-                codeAnimation.innerHTML = javaCode.substring(0, i - 1);
+            if (i > 0) {
+                codeAnimation.innerHTML = Prism.highlight(
+                    javaCode.substring(0, i - 1), 
+                    Prism.languages.java, 
+                    'java'
+                );
                 i--;
                 setTimeout(typeAndDelete, 20);
-            }
-            else{
+            } else {
                 deleting = false;
                 setTimeout(typeAndDelete, 500);
             }
